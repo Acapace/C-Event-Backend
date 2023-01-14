b0f5fe666bbeb553ba94520225e3bd7f8b25f232
Ashley Capace <acapace@yahoo.com>
2023-01-13 23:53:57 -0500

    backend startup

---
+++ django_car_events_api/car_events_api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Blog
+from .models import CarSpecs
+from .models import CarPlan
+from .models import Inventory
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ('id', 'date', 'event_name',
+                  'event_location', 'topic', 'post')
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = ('id', 'vendor_name', 'item_name',
+                  'item_description', 'item_price', 'image_link')
+
+
+class CarPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarPlan
+        fields = ('id', 'plan_name', 'year_of_warranty', 'finance_plan')
+
+
+class CarSpecsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarSpecs
+        fields = ('id', ' production_year', ' car_brand',
+                  'car_model', 'engine_type', 'image_link')
