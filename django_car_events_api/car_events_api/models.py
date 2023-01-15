31b80837969582d479a2d259b2962ba64fcf4a66
Ashley Capace <acapace@yahoo.com>
2023-01-15 13:25:11 -0500

    update

--- django_car_events_api/car_events_api/models.py
+++ django_car_events_api/car_events_api/models.py
@@ -20,21 +20,25 @@ class Inventory(models.Model):
     image_link = models.CharField(max_length=255, blank=True)
 
 
-class CarPlan(models.Model):
-    plan_name = models.CharField(max_length=20)
-    year_of_warranty = models.PositiveBigIntegerField(default=1)
-    finance_plan = models.CharField(max_length=20, default="unavailable")
-
-    def __str__(self):
-        return self.plan_name
-
-
 class CarSpecs(models.Model):
-    production_year = models.CharField(max_length=20)
-    car_brand = models.CharField(max_length=60)
+    # car_plan = models.ForeignKey(
+    #     CarPlan, on_delete=models.SET_NULL, null=True)
+    production_year = models.IntegerField()
+    car_make = models.CharField(max_length=60)
     car_model = models.CharField(max_length=100)
     engine_type = models.CharField(max_length=50)
     image_link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.car_model
+
+
+class CarPlan(models.Model):
+    car_specs = models.ForeignKey(
+        CarSpecs, related_name='plans', on_delete=models.CASCADE, null=True)
+    plan_name = models.CharField(max_length=20)
+    year_of_warranty = models.PositiveBigIntegerField(default=1)
+    finance_plan = models.CharField(max_length=20, default="unavailable")
+
+    def __str__(self):
+        return self.plan_name
--- django_car_events_api/car_events_api/serializers.py
+++ django_car_events_api/car_events_api/serializers.py
@@ -5,28 +5,32 @@ from .models import CarPlan
 from .models import Inventory
 
 
-class BlogSerializer(serializers.ModelSerializer):
+class BlogSerializer(serializers.Serializer):
     class Meta:
         model = Blog
         fields = ('id', 'date', 'event_name',
                   'event_location', 'topic', 'post')
 
 
-class InventorySerializer(serializers.ModelSerializer):
+class InventorySerializer(serializers.Serializer):
     class Meta:
         model = Inventory
         fields = ('id', 'vendor_name', 'item_name',
                   'item_description', 'item_price', 'image_link')
 
 
-class CarPlanSerializer(serializers.ModelSerializer):
+class CarSpecsSerializer(serializers.Serializer):
+    plans = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='plan_name')
+
+    class Meta:
+        model = CarSpecs
+        fields = ('id', 'plans', 'production_year', 'car_make',
+                  'car_model', 'engine_type', 'image_link')
+        # depth = 1
+
+
+class CarPlanSerializer(serializers.Serializer):
     class Meta:
         model = CarPlan
         fields = ('id', 'plan_name', 'year_of_warranty', 'finance_plan')
-
-
-class CarSpecsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CarSpecs
-        fields = ('id', ' production_year', ' car_brand',
-                  'car_model', 'engine_type', 'image_link')
